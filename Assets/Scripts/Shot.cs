6c66ed8480b2f3dc44781ce93c2e31a776f484d9
Grayson1509 <alessiomanna.job@gmail.com>
2019-12-27 01:37:19 +0100

    /. Polishing Workspace

--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -39,6 +39,7 @@ public class Shot : MonoBehaviour
     }
 
     void DestroyAsteroid()
+
     {
 
     }
---
+++ Assets/src/System/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    [Header("References")]
+    public GameObject PlayerShip;
+
+    [Header("Game Rules")]
+    public Vector2 ScreenResolution;
+    public int CurrentPoints;
+    public int PointsToNextLevel;
+    public int currentlevel;
+
+    public void Awake()
+    {
+
+        //<---Read the screen resolution and sets the screen limit
+        //<---Load Level Achived
+        //<---try to find the ship - if found reference else create
+
+    }
+
+    public void Update()
+    {
+        //<---Exausted Lifes?
+        //<---control player position
+        //<---player hit something
+        //<---Update Points
+        //<---Update UI
+    }
+
+  
+
+}
+
+
