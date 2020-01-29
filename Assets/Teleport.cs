b33fbfd3083363c2b40c189811256ff71f928eba
Tiger1301 <comotti.daniele@gmail.com>
2020-01-29 10:04:25 +0100

    Il poco che sono riuscito a fare

---
+++ Assets/Teleport.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform LimitDown;
+    public GameObject Ship;
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Limit")
+        {
+            transform.Translate(0f, 0f, -25f);
+        }
+    }
+}
---
+++ Assets/Teleport1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport1 : MonoBehaviour
+{
+    public Transform LimitUp;
+    public GameObject Ship;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "LimitDown")
+        {
+            transform.Translate(0f, 0f, 31f);
+        }
+    }
+}
